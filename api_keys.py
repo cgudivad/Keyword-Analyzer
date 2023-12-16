3517adf07565b18e185c18f56b1117f2c1409dc0
cgudivad <126507537+cgudivad@users.noreply.github.com>
2023-12-15 20:43:44 -0600

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,4 +1,4 @@
-open_AI_GPT_model = "gpt-3.5-turbo-1106"
-open_AI_API_key = "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"
-cx_key = '03a9bc3a9d89245a3'
-google_api_key = 'AIzaSyDzh2Zln7Y_7WCY20HW9QzKIEZOejhpask'
+open_AI_GPT_model = "insert-chat-gpt-model-here"
+open_AI_API_key = "insert-openai-api-key-here"
+cx_key = 'insert-google-search-api-cx-key-here'
+google_api_key = 'insert-api-key-here'
